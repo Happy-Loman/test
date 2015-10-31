aeee31c7959edd0d768fbdd2b42017cc5492602f
Happy-Loman <mrhappyloman@gmail.com>
2015-10-31 16:26:15 -0400

    Update webworker.js

--- webworker.js
+++ webworker.js
@@ -1,13 +1,1 @@
-var i = 0;
 
-/*function timedCount() {
-    i = i + 1;
-    postMessage(i);
-    setTimeout("timedCount()",500);
-}
-
-timedCount();*/
-window.setInterval(function(){
-    i = i + 1;
-    postMessage(i);
-}, 1);
